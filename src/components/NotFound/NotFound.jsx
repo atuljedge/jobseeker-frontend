871d9882bbdafacbf8e192297f417070305d5a26
Atul jedge <atuljedge2801@gmail.com>
2024-05-22 12:25:51 +0530

    changes commited

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,16 @@
+import {Link} from 'react-router-dom';
+// import 'app.css'
+
+const NotFound = () => {
+  return (
+
+      <section className="page notfound">
+          <div className="content">
+            <img src="/notfound.png" alt="notfound" />
+            <Link to={"/"}>Return to Home</Link>
+          </div>
+      </section>
+  );
+};
+
+export default NotFound
